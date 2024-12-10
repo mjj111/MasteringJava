92c6d092fb48ecd483c067b6734be55ca8acf08b
mjj111 <skatks1016@naver.com>
2024-12-10 15:09:58 +0900

    var 선언으로 일관성 유지

--- src/test/StringTest.java
+++ src/test/StringTest.java
@@ -11,15 +11,15 @@ public class StringTest extends AutoTester {
     @Test
     public void 문자_빈도수_세기(){
         var GIVEN_INPUT = new CountFrequencyOfCharacter.Dto("aexxxi", 'i');
-        Long EXPECTED_OUTPUT = 1L;
+        var EXPECTED_OUTPUT = 1L;
 
         testMethods(CountFrequencyOfCharacter.class, GIVEN_INPUT, EXPECTED_OUTPUT);
     }
 
     @Test
     public void 숫자만_포함하는_문자열인지_확인(){
-        String GIVEN_RIGHT_INPUT = "123123";
-        String GIVEN_WRONG_INPUT = "sdk124125";
+        var GIVEN_RIGHT_INPUT = "123123";
+        var GIVEN_WRONG_INPUT = "sdk124125";
 
         testMethods(ContainsOnlyDigit.class, GIVEN_RIGHT_INPUT, true);
         testMethods(ContainsOnlyDigit.class, GIVEN_WRONG_INPUT, false);
@@ -27,8 +27,8 @@ public class StringTest extends AutoTester {
 
     @Test
     public void 문자열에서_문자_빈도수를_반환(){
-        String GIVEN_INPUT = "asdqbaa";
-        Map<Character, Long> EXPECTED_OUTPUT = Map.of('a',3L,
+        var GIVEN_INPUT = "asdqbaa";
+        var EXPECTED_OUTPUT = Map.of('a',3L,
                 's',1L,
                 'd',1L,
                 'q',1L,
@@ -39,7 +39,7 @@ public class StringTest extends AutoTester {
 
     @Test
     public void 자음과_모음_개수_세기() {
-        String GIVEN_INPUT = "aexxxi";
+        var GIVEN_INPUT = "aexxxi";
         var EXPECTED_OUTPUT = CountVowelsAndConsonants.Pair.of(3L,3L);
 
         testMethods(CountVowelsAndConsonants.class, GIVEN_INPUT, EXPECTED_OUTPUT);
@@ -48,7 +48,7 @@ public class StringTest extends AutoTester {
     @Test
     public void 반복되지_않는_첫_번째_문자_찾기() {
         var GIVEN_INPUT = "aarc";
-        char EXPECTED_OUTPUT = 'r';
+        var EXPECTED_OUTPUT = 'r';
 
         testMethods(FirstNonRepeatedCharacter.class, GIVEN_INPUT, EXPECTED_OUTPUT);
     }
@@ -56,7 +56,7 @@ public class StringTest extends AutoTester {
     @Test
     public void 문자열_뒤집기() {
         var GIVEN_INPUT = "12441 12441 12441 12441 12441 12441 12441";
-        var  EXPECTED_OUTPUT = "14421 14421 14421 14421 14421 14421 14421";
+        var EXPECTED_OUTPUT = "14421 14421 14421 14421 14421 14421 14421";
 
         testMethods(ReverseWords.class, GIVEN_INPUT, EXPECTED_OUTPUT);
     }
