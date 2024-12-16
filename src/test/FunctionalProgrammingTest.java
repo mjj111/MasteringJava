97906a69cdbcf2ed062df57ea3c53e9fe6ef26e7
mjj111 <skatks1016@naver.com>
2024-12-16 21:01:00 +0900

    테스트 코드 추가

--- src/test/FunctionalProgrammingTest.java
+++ src/test/FunctionalProgrammingTest.java
@@ -1,15 +1,19 @@
 package src.test;
 
 import org.junit.jupiter.api.Test;
+import src.main.functional_programming.FindFirstAndLast;
 import src.main.functional_programming.ReduceString;
 import src.main.functional_programming.ReplaceList;
+import src.main.functional_programming.RandomStringFromStrings;
 
 import java.util.Arrays;
 import java.util.function.Function;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-public class FuntionalProgrammingTest {
+public class FunctionalProgrammingTest {
+
     @Test
     void 리스트의_각_문자열에_함수_적용() {
         var GIVEN_INPUT = Arrays.asList("김명준은 손가락이 10개다.", "1 2 반갑습니다.");
@@ -27,5 +31,23 @@ public class FuntionalProgrammingTest {
         Function<String, String> f = ReduceString.reduceStrings(f1, f2);
 
         assertEquals("TEST DONE", f.apply("test"));
+    }
+
+    @Test
+    void 첫_번째와_마지막_추출(){
+        var GIVEN_INPUT = "Lambda";
+        var EXPECTED_OUTPUT = FindFirstAndLast.firstAndLastChar.apply(GIVEN_INPUT);
+
+        assertEquals("La", EXPECTED_OUTPUT);
+    }
+
+    @Test
+    public void 문자열_랜덤_하나추출() {
+        var GIVEN_INPUT = Arrays.asList("Temporary", "Random", "Text");
+
+        var result = RandomStringFromStrings.extract(GIVEN_INPUT);
+
+        assertEquals(GIVEN_INPUT.size(), result.size());
+        assertNotEquals(GIVEN_INPUT, result);
     }
 }
